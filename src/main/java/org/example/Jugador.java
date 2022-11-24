cb5cbf78fbd8554745080b17b375bbb37663c13b
Darka304 <esteban.1123@hotmail.com>
2022-11-24 11:32:20 -0500

    examen

--- src/main/java/org/example/Jugador.java
+++ src/main/java/org/example/Jugador.java
@@ -2,11 +2,82 @@ package org.example;
 
 public class Jugador {
 
-    String nombre="James";
-    int edad;
-    double apellidos;
+   public String nombre;
+   public int edad;
+   public String apellidos;
+   public int numerodecamiseta;
+   public String posicion;
+   public String equipo;
 
-    public Jugador(String nombre) {
+//CONTRUTOR
+
+
+    public Jugador() {
+    }
+
+    public Jugador(String nombre, int edad, String apellidos, int numerodecamiseta, String posicion, String equipo) {
         this.nombre = nombre;
+        this.edad = edad;
+        this.apellidos = apellidos;
+        this.numerodecamiseta = numerodecamiseta;
+        this.posicion = posicion;
+        this.equipo = equipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getNumerodecamiseta() {
+        return numerodecamiseta;
+    }
+
+    public void setNumerodecamiseta(int numerodecamiseta) {
+        this.numerodecamiseta = numerodecamiseta;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+
+    public void setApellido(int next) {
+    }
+
+    public void setEquipoDondeJuega(int next) {
+    }
+
+    public void setNumeroCamiseta(int nextInt) {
     }
 }
